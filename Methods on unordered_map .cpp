f2e07a7ac25adcef0029aad15f9e4a1ab7eb9b1c
Ruhul Amin <ruhul.computer12@gmail.com>
2024-06-18 19:17:05 +0600

    Git various function practice.

--- Methods on unordered_map .cpp
+++ Methods on unordered_map .cpp
@@ -4,6 +4,32 @@
 using namespace std;
 
 int main(){
+    unordered_map<string, double>map = {
+    {"One", 1}, {"Two", 2}, {"Three", 3}
+    };
+
+    map["PI"] = 3.1416;
+    map["root2"] = 1.414;
+    map["root3"] = 1.732;
+    map["log10"] = 2.302;
+    map["loge"] = 1.0;
+
+    string key = "FI";
+
+    if(map.find(key) != map.end()){
+        cout << "The value of "<< key << " is : " << map[key] << endl;
+    }
+    else {
+        cout << "The value of " << key << " is not found." << endl;
+    }
+
+    //Retrive all elements using iterator
+
+    unordered_map<string, double> :: iterator itr;
+    cout << "All elements are: \n";
+    for(itr = map.begin(); itr != map.end(); itr++){
+        cout << itr->first << " " << itr->second << "\n";
+    }
 
     return 0;
 }
